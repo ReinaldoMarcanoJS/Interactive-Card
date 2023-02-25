40ba794a88045b1fb9337bdabcc58a8604d9ab83
Reinaldo Marcano <reynaldomarcano4@gmail.com>
2023-02-25 16:44:43 -0400

    Login-component

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import "./index.css"
 
-function Login({children}) {
+function Login({
+    children,
+    formLogin,
+    setFormLogin,
+    error,
+    setError,
+    loading,
+    setLoading,
+    sentForm,
+    setSentForm
+    }) {
+
   return (
     <div className='Content-children'>
-        {children}
+        {
+          React.Children
+          .toArray(children)
+          .map(child => React.cloneElement(child, {  
+            formLogin,
+            setFormLogin,
+            error,
+            setError,
+            loading,
+            setLoading,
+            sentForm,
+            setSentForm,
+          })) 
+        }
     </div>
   )
 }
