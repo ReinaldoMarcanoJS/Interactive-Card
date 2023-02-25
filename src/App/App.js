f613739869b658fd0b62d9de682c4c9480ce567b
Reinaldo Marcano <reynaldomarcano4@gmail.com>
2023-02-24 23:43:44 -0400

    App Component

--- src/App/App.js
+++ src/App/App.js
@@ -2,14 +2,41 @@ import './App.css';
 import { FormData } from '../FormData';
 import { VisualCard } from '../VisualCard';
 import { Login } from '../Login';
+import { useTodos } from '../App/useTodos'
 import React from 'react';
+
+
+
 function App() {
+
+  const {
+    formLogin,
+    setFormLogin,
+    error,
+    setError,
+    loading,
+    setLoading,
+    sentForm,
+    setSentForm,
+  } = useTodos()
   return (
     <React.Fragment>
 
     <Login>
-      <VisualCard/>
-      <FormData/>
+      <VisualCard
+        formLogin={formLogin}
+        loading={loading}
+        setLoading={setLoading}
+        sentForm={sentForm}
+        setSentForm={setSentForm}
+      />
+      <FormData
+        setFormLogin={setFormLogin}
+        setLoading={setLoading}
+        sentForm={sentForm}
+        setSentForm={setSentForm}
+        setError={setError}
+      />
     </Login>
       
     </React.Fragment>
