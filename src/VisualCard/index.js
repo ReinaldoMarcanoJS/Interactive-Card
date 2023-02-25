c8da2dbca09848d24e2d3cef55a510bbfa16616d
Reinaldo Marcano <reynaldomarcano4@gmail.com>
2023-02-24 23:41:33 -0400

    Visual Card

--- src/VisualCard/index.js
+++ src/VisualCard/index.js
@@ -2,22 +2,25 @@ import React from 'react'
 import Card_front from "../image/bg-card-front.png"
 import Card_back from "../image/bg-card-back.png"
 import "./index.css"
-function VisualCard() {
+function VisualCard({formLogin,loading,setLoading,sentForm}) {
+
+  
+
   return (
     <React.Fragment>
         <div className='div-content'>
         <div className='div-cards'>
           <img src={Card_back} className="img-card-back"></img>
           <img src={Card_front} className="img-card-front"></img>
-          <p className='p-back-card'>000</p>
-          <div className='div-front-pic'></div>
-            <div className='div-front-pictwo'></div>
-            <p className='p-front-card-name'>JANE APPLESEED</p>
-            <p className='p-front-card-data'>00/00</p>
-            <div className='div-p-numbers'>
-            <p className='p-front-card-numbers'>0000 0000 0000 0000</p>
+            <p className='p-back-card'>{sentForm ? formLogin.cvc : "000"  } </p>
+            <div className='div-front-pic'></div>
+              <div className='div-front-pictwo'></div>
+              <p className='p-front-card-name'>{sentForm ? formLogin.name : "JANE APPLESEED"  }</p>
+              <p className='p-front-card-data'>{sentForm ?  formLogin.MM + "/" + formLogin.YY : "00/00"  }</p>
+              <div className='div-p-numbers'>
+              <p className='p-front-card-numbers'>{sentForm ? formLogin.number_card :  "0000 0000 0000 0000"}</p>
 
-            </div>
+          </div>
         </div>
           
             
